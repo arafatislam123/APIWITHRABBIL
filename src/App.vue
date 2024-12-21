c6e759012a7b5206e70e7fe623e0e93fedd5c4ca
arafatislam123 <arafatpstu7@gmail.com>
2024-12-22 01:55:18 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+
+<script setup>
+import { ref } from 'vue';
+const products = ref([]);
+
+CallApi();
+async function CallApi() {
+  const response = await fetch('https://dummyjson.com/products');
+  const data = await response.json();
+  products.value = data.products; // Access the products array
+  console.log(data);
+}
+</script>
+
+<template>
+  <table>
+    <thead>
+    <tr>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Price</th>
+      <th>Category</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="product in products" :key="product.id">
+      <td>{{ product.id }}</td>
+      <td>{{ product.title }}</td>
+      <td>{{ product.price }}</td>
+      <td>{{ product.category }}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+</style>
